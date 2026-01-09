59d02dabd04aa64111cb66cd513c6bb02066a658
Zihua Chen <chenzihu@shernet.sheridancollege.ca>
2026-01-09 17:46:28 -0500

    22

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,43 +3,44 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float speed = 0.02f;
-    public float xMax = 10f;
-    public float xMin = -10f;
-
-    bool movingRight = true;
-
+    public float xMax;
+    public float xMin;
+    public Camera gameCamera;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 moverXPos = transform.position;
-        if (movingRight)
-        {
-            moverXPos.x = moverXPos.x + speed;
-            
-            if (moverXPos.x > xMax) 
-            {
-                movingRight = false;
-            }
-        }
-        else
-        {
-            {
-                moverXPos.x = moverXPos.x - speed;
-                
-                if (moverXPos.x < xMin)
-                {
-                    movingRight = true;
-                }
-            }
-        }
+        moverXPos.x = moverXPos.x + speed;
         transform.position = moverXPos;
+
+        //Screen.width;
+        //gameCamera.WorldToScreenPoint(//somerandomvector);
+
+        //set xMax to wherever is too far to the right for the player to see
+        Vector3 screenTransformPosition = gameCamera.WorldToScreenPoint(transform.position);
+        xMax = Screen.width;
+
+        //set xMin to wherever is too far to the left for the player to see
+        xMin = 0;
+
+
+        if (xMax < screenTransformPosition.x)
+        {
+            speed *= -1;
+        }
+
+        if (xMin > screenTransformPosition.x)
+        {
+            speed *= -1;
+        }
+
     }
 }
