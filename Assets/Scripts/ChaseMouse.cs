2ecf54a5ece211a8a4d17e4cdf45817c72731d90
HHHXLK <2639580737hxlk@gmail.com>
2026-01-29 21:35:40 -0500

    Code: ChaseMouse

---
+++ Assets/Scripts/ChaseMouse.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using UnityEngine;
+
+public class ChaseMouse : MonoBehaviour
+{
+    public Transform bugTarget;
+    public float followSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 current = transform.position;
+        Vector3 target = bugTarget.position;
+
+        float t = followSpeed * Time.deltaTime;
+
+      //Take a midpoint and have the frog gradually approach the insect.
+      transform.position = Vector3.Lerp(current, target, t);
+    }
+}
